ff9aed8038efef8a6671cc94ab8aaa5accbe5c32
lfyuomr-gylo <lfyuomr-gylo@yandex.ru>
2022-03-08 13:59:30 +0400

    Release script

---
+++ release.sh
@@ -0,0 +1,7 @@
+set -x
+
+RELEASE=0.1
+env GOOS=darwin GOARCH=amd64 go build -o build/anki-helper-v${RELEASE}-darwin-amd64 .
+env GOOS=darwin GOARCH=arm64 go build -o build/anki-helper-v${RELEASE}-darwin-arm64 .
+env GOOS=linux GOARCH=amd64 go build -o build/anki-helper-v${RELEASE}-linux-amd64 .
+env GOOS=windows GOARCH=amd64 go build -o build/anki-helper-v${RELEASE}-windows-amd64.exe .
