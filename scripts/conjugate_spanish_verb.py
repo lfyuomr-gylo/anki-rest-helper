c0711ff74d21648672f7bc41ab67b1d385524cab
Lev Khotov <lfyuomr-gylo@yandex.ru>
2023-06-10 15:58:58 +0200

    Add conjugation for perfect and progressive tenses.

--- scripts/conjugate_spanish_verb.py
+++ scripts/conjugate_spanish_verb.py
@@ -27,6 +27,7 @@ class ConjugationRule:
   sd_pronoun: str
   sd_paradigm: str
   sd_tense: str = None
+  iprob: float = 1 # number in range [0; 1]; probability of the conjugation card generation for irregular verbs
 
   def produce_note_modifications(self, sd_conjugation, note_tags):
     if self._done_tag() in note_tags:
@@ -37,10 +38,7 @@ class ConjugationRule:
       if self._matches(conj):
         modifications = [{'add_tag': self._done_tag()}]
 
-        prob = self.prob
-        if conj.get('isIrregular', 'False'):
-          # we always generate conjugation card if the conjugation form is irregular
-          prob = 1
+        prob = self.iprob if conj.get('isIrregular', False) else self.prob
         if random.random() < prob:
           modifications.append({
             'set_field': {self.note_field: conj.get('word', '')}
@@ -65,30 +63,43 @@ class ConjugationRule:
 
 
 RULES = [
-      # Anki field |
-      ConjugationRule(note_field="IndicativePresentYo",        prob=0.05, sd_pronoun="yo",               sd_paradigm="presentIndicative"),
-      ConjugationRule(note_field="IndicativePresentTu",        prob=0.30, sd_pronoun="tú",               sd_paradigm="presentIndicative"),
-      ConjugationRule(note_field="IndicativePresentEl",        prob=0.30, sd_pronoun="él/ella/Ud.",      sd_paradigm="presentIndicative"),
-      ConjugationRule(note_field="IndicativePresentNosotros",  prob=0.10, sd_pronoun="nosotros",         sd_paradigm="presentIndicative"),
-      ConjugationRule(note_field="IndicativePresentVosotros",  prob=0.10, sd_pronoun="vosotros",         sd_paradigm="presentIndicative"),
-      ConjugationRule(note_field="IndicativePresentEllos",     prob=0.15, sd_pronoun="ellos/ellas/Uds.", sd_paradigm="presentIndicative"),
+      ConjugationRule(note_field="IndicativePresentYo",        prob=0.05,             sd_pronoun="yo",               sd_paradigm="presentIndicative"),
+      ConjugationRule(note_field="IndicativePresentTu",        prob=0.30,             sd_pronoun="tú",               sd_paradigm="presentIndicative"),
+      ConjugationRule(note_field="IndicativePresentEl",        prob=0.30,             sd_pronoun="él/ella/Ud.",      sd_paradigm="presentIndicative"),
+      ConjugationRule(note_field="IndicativePresentNosotros",  prob=0.10,             sd_pronoun="nosotros",         sd_paradigm="presentIndicative"),
+      ConjugationRule(note_field="IndicativePresentVosotros",  prob=0.10,             sd_pronoun="vosotros",         sd_paradigm="presentIndicative"),
+      ConjugationRule(note_field="IndicativePresentEllos",     prob=0.15,             sd_pronoun="ellos/ellas/Uds.", sd_paradigm="presentIndicative"),
 
-      ConjugationRule(note_field="ImperativeAffirmativeTu",    prob=0.10, sd_pronoun="tú",               sd_paradigm="imperative", sd_tense="affirmative"),
-      ConjugationRule(note_field="ImperativeAffirmativeUsted", prob=0.10, sd_pronoun="Ud.",              sd_paradigm="imperative", sd_tense="affirmative"),
+      ConjugationRule(note_field="ImperativeAffirmativeTu",    prob=0.10,             sd_pronoun="tú",               sd_paradigm="imperative", sd_tense="affirmative"),
+      ConjugationRule(note_field="ImperativeAffirmativeUsted", prob=0.10,             sd_pronoun="Ud.",              sd_paradigm="imperative", sd_tense="affirmative"),
 
-      ConjugationRule(note_field="PreteriteYo",                prob=0.30, sd_pronoun="yo",               sd_paradigm="preteritIndicative"),
-      ConjugationRule(note_field="PreteriteTu",                prob=0.10, sd_pronoun="tú",               sd_paradigm="preteritIndicative"),
-      ConjugationRule(note_field="PreteriteEl",                prob=0.30, sd_pronoun="él/ella/Ud.",      sd_paradigm="preteritIndicative"),
-      ConjugationRule(note_field="PreteriteNosotros",          prob=0.05, sd_pronoun="nosotros",         sd_paradigm="preteritIndicative"),
-      ConjugationRule(note_field="PreteriteVosotros",          prob=0.05, sd_pronoun="vosotros",         sd_paradigm="preteritIndicative"),
-      ConjugationRule(note_field="PreteriteEllos",             prob=0.20, sd_pronoun="ellos/ellas/Uds.", sd_paradigm="preteritIndicative"),
+      ConjugationRule(note_field="PreteriteYo",                prob=0.30,             sd_pronoun="yo",               sd_paradigm="preteritIndicative"),
+      ConjugationRule(note_field="PreteriteTu",                prob=0.10,             sd_pronoun="tú",               sd_paradigm="preteritIndicative"),
+      ConjugationRule(note_field="PreteriteEl",                prob=0.30,             sd_pronoun="él/ella/Ud.",      sd_paradigm="preteritIndicative"),
+      ConjugationRule(note_field="PreteriteNosotros",          prob=0.05,             sd_pronoun="nosotros",         sd_paradigm="preteritIndicative"),
+      ConjugationRule(note_field="PreteriteVosotros",          prob=0.05,             sd_pronoun="vosotros",         sd_paradigm="preteritIndicative"),
+      ConjugationRule(note_field="PreteriteEllos",             prob=0.20,             sd_pronoun="ellos/ellas/Uds.", sd_paradigm="preteritIndicative"),
 
-      ConjugationRule(note_field="ImperfectYo",                prob=0.15, sd_pronoun="yo",               sd_paradigm="imperfectIndicative"),
-      ConjugationRule(note_field="ImperfectTu",                prob=0.20, sd_pronoun="tú",               sd_paradigm="imperfectIndicative"),
-      ConjugationRule(note_field="ImperfectEl",                prob=0.15, sd_pronoun="él/ella/Ud.",      sd_paradigm="imperfectIndicative"),
-      ConjugationRule(note_field="ImperfectNosotros",          prob=0.15, sd_pronoun="nosotros",         sd_paradigm="imperfectIndicative"),
-      ConjugationRule(note_field="ImperfectVosotros",          prob=0.15, sd_pronoun="vosotros",         sd_paradigm="imperfectIndicative"),
-      ConjugationRule(note_field="ImperfectEllos",             prob=0.20, sd_pronoun="ellos/ellas/Uds.", sd_paradigm="imperfectIndicative"),
+      ConjugationRule(note_field="ImperfectYo",                prob=0.15,             sd_pronoun="yo",               sd_paradigm="imperfectIndicative"),
+      ConjugationRule(note_field="ImperfectTu",                prob=0.20,             sd_pronoun="tú",               sd_paradigm="imperfectIndicative"),
+      ConjugationRule(note_field="ImperfectEl",                prob=0.15,             sd_pronoun="él/ella/Ud.",      sd_paradigm="imperfectIndicative"),
+      ConjugationRule(note_field="ImperfectNosotros",          prob=0.15,             sd_pronoun="nosotros",         sd_paradigm="imperfectIndicative"),
+      ConjugationRule(note_field="ImperfectVosotros",          prob=0.15,             sd_pronoun="vosotros",         sd_paradigm="imperfectIndicative"),
+      ConjugationRule(note_field="ImperfectEllos",             prob=0.20,             sd_pronoun="ellos/ellas/Uds.", sd_paradigm="imperfectIndicative"),
+
+      ConjugationRule(note_field="PerfectYo",                  prob=0.15, iprob=0.25, sd_pronoun="yo",               sd_paradigm="presentPerfect"),
+      ConjugationRule(note_field="PerfectTu",                  prob=0.15, iprob=0.25, sd_pronoun="tú",               sd_paradigm="presentPerfect"),
+      ConjugationRule(note_field="PerfectEl",                  prob=0.15, iprob=0.25, sd_pronoun="él/ella/Ud.",      sd_paradigm="presentPerfect"),
+      ConjugationRule(note_field="PerfectNosotros",            prob=0.15, iprob=0.25, sd_pronoun="nosotros",         sd_paradigm="presentPerfect"),
+      ConjugationRule(note_field="PerfectVosotros",            prob=0.15, iprob=0.25, sd_pronoun="vosotros",         sd_paradigm="presentPerfect"),
+      ConjugationRule(note_field="PerfectEllos",               prob=0.15, iprob=0.25, sd_pronoun="ellos/ellas/Uds.", sd_paradigm="presentPerfect"),
+
+      ConjugationRule(note_field="ProgressiveYo",              prob=0.15, iprob=0.25, sd_pronoun="yo",               sd_paradigm="presentContinuous"),
+      ConjugationRule(note_field="ProgressiveTu",              prob=0.15, iprob=0.25, sd_pronoun="tú",               sd_paradigm="presentContinuous"),
+      ConjugationRule(note_field="ProgressiveEl",              prob=0.15, iprob=0.25, sd_pronoun="él/ella/Ud.",      sd_paradigm="presentContinuous"),
+      ConjugationRule(note_field="ProgressiveNosotros",        prob=0.15, iprob=0.25, sd_pronoun="nosotros",         sd_paradigm="presentContinuous"),
+      ConjugationRule(note_field="ProgressiveVosotros",        prob=0.15, iprob=0.25, sd_pronoun="vosotros",         sd_paradigm="presentContinuous"),
+      ConjugationRule(note_field="ProgressiveEllos",           prob=0.15, iprob=0.25, sd_pronoun="ellos/ellas/Uds.", sd_paradigm="presentContinuous"),
 ]
 
 if __name__ == '__main__':
